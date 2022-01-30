50f6b3458f69ab155508714914f818d7061654e2
Suraj Mishra <mishrasur7@gmail.com>
2022-01-30 22:41:30 +0200

    first thymeleaf succeeded

--- Exercise2/src/main/java/com/example/Exercise2/web/HelloThymeleafController.java
+++ Exercise2/src/main/java/com/example/Exercise2/web/HelloThymeleafController.java
@@ -1,16 +1,16 @@
 package com.example.Exercise2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloThymeleafController {
-
-@RequestMapping("hello") 
-public String helloThymeleaf(@RequestParam String name, int age) {
-	return "hellothymeleaf"; 
-}
-	
-
+ @RequestMapping("/hello")
+public String greeting(@RequestParam("name") String name, @RequestParam("age") int age,  Model model) {
+ model.addAttribute("name", name);
+ model.addAttribute("age", age); 
+ return "hellothymeleaf";
+ }
 }
