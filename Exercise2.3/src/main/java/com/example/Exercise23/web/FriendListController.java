6152e1bf6dbefe86f5c9d29356c9a0e2a61c8f82
Suraj Mishra <mishrasur7@gmail.com>
2022-02-01 20:20:27 +0200

    stucked

--- Exercise2.3/src/main/java/com/example/Exercise23/web/FriendListController.java
+++ Exercise2.3/src/main/java/com/example/Exercise23/web/FriendListController.java
@@ -4,25 +4,29 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.Exercise23.model.Friend;
 
 @Controller
 public class FriendListController {
+	ArrayList<Friend> friends = new ArrayList<>();
+	
+	Friend friend1 = new Friend("Suraj Mishra"); 
+	Friend friend2 = new Friend("Alpo remes"); 
+	
+	
+
+	
 
 	@RequestMapping("/index")
 	public String friendList(Friend friend, Model model) {
-		Friend friend1 = new Friend("Suraj Mishra");
-		Friend friend2 = new Friend("Shova Pandey");
-
-		ArrayList<Friend> friends = new ArrayList<Friend>();
-
-		friends.add(friend1);
-		friends.add(friend2);
-		friends.add(new Friend(friend.getName())); 
+		friends.add(friend);
 
 		model.addAttribute("friends", friends);
+
 		return "friendlist";
 	}
+
 }
