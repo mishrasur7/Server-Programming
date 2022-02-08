364b37e882c9372d1f178a053392abf638fa7c78
Suraj Mishra <mishrasur7@gmail.com>
2022-02-08 15:54:06 +0200

    booklist view added

--- surajbookstore/src/main/java/com/example/surajbookstore/web/BookController.java
+++ surajbookstore/src/main/java/com/example/surajbookstore/web/BookController.java
@@ -4,21 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.surajbookstore.model.Book;
 import com.example.surajbookstore.model.BookRepository;
 
 @Controller
-@ResponseBody
 public class BookController {
 	@Autowired
 	private BookRepository repository; 
 	
-	@GetMapping("/index")
-	public String books() {
-		List<Book> books = repository.findByTitle(null); 
-		return "placeholder";
+	@RequestMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll()); 
+		return "booklist";
 	}
 }
