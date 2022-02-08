72fe5db30ad769714fbad37c73fbf39589ed0ed3
Suraj Mishra <mishrasur7@gmail.com>
2022-02-08 17:55:59 +0200

    data shown to browser

--- surajbookstore/src/main/java/com/example/surajbookstore/SurajbookstoreApplication.java
+++ surajbookstore/src/main/java/com/example/surajbookstore/SurajbookstoreApplication.java
@@ -14,18 +14,13 @@ public class SurajbookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SurajbookstoreApplication.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner studentDemo(BookRepository repository) {
 		return (args) -> {
-			System.out.println("save a couple of students");
-			repository.save(new Book("Introduction to Spring Boot", "Jukka", 2000, "ISBN123456", 23.34));
-			repository.save(new Book("Introduction to React", "Juha", 2001, "ISBN123321", 22.99));	
-			
-			System.out.println("fetch all students");
-			for (Book book : repository.findAll()) {
-				System.out.println(book.toString());
-			}
+
+			repository.save(new Book("Introduction to Spring Boot", "Jukka Juslin", 2000, "ISBN123456", 23.34));
+			repository.save(new Book("Introduction to React", "Juha Hinkula", 2001, "ISBN123321", 22.99));
 
 		};
 	}
