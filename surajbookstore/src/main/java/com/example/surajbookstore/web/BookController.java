718489cc64d11ad45a18b4e1b6f6fdca759a5e83
Suraj Mishra <mishrasur7@gmail.com>
2022-02-08 14:39:28 +0200

    crud started

--- surajbookstore/src/main/java/com/example/surajbookstore/web/BookController.java
+++ surajbookstore/src/main/java/com/example/surajbookstore/web/BookController.java
@@ -1,14 +1,24 @@
 package com.example.surajbookstore.web;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.surajbookstore.model.Book;
+import com.example.surajbookstore.model.BookRepository;
+
 @Controller
 @ResponseBody
 public class BookController {
-@RequestMapping(name="/index")
-public String books() {
-return "placeholder";
-}
+	@Autowired
+	private BookRepository repository; 
+	
+	@GetMapping("/index")
+	public String books() {
+		List<Book> books = repository.findByTitle(null); 
+		return "placeholder";
+	}
 }
